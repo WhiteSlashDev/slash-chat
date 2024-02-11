092673c9494b54146ef51b2f97e2aead2d375072
WhiteSlashDev <white.slash@mail.ru>
2024-02-12 01:32:22 +0300

    added history

--- src/Events/MessageCreate.ts
+++ src/Events/MessageCreate.ts
@@ -1,10 +1,10 @@
 import { RecipleModuleLoadData } from "reciple";
 import { BaseModule } from "../BaseModule.js";
 import Utility from "../Utils/Utility.js";
-import { EmbedBuilder, Message } from "discord.js";
-import { GoogleGenerativeAI } from "@google/generative-ai";
+import { BaseMessageOptions, EmbedBuilder, Message } from "discord.js";
+import axios from "axios";
 import { ChatHistory } from "@prisma/client";
-const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY || "");
+import { IncomingMessage } from "http";
 
 export class MessageCreate extends BaseModule {
   public async onStart(): Promise<boolean> {
@@ -35,6 +35,10 @@ export class MessageCreate extends BaseModule {
             text: `${Utility.client.user.username}`,
           })
           .setColor(Utility.embedColor)
+          .setAuthor({
+            name: message.author.displayName,
+            iconURL: message.author.avatarURL() || "",
+          })
           .setTimestamp();
 
         const response = await message.reply({
@@ -57,28 +61,58 @@ export class MessageCreate extends BaseModule {
     embed: EmbedBuilder,
     chatId: string
   ) {
-    const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+    await this.addChatHistory(chatId, "user", input);
 
-    const chatHistories = await this.GetAllChatHistory(chatId);
+    const url = `https://api.nvcf.nvidia.com/v2/nvcf/pexec/functions/0e349b44-440a-44e1-93e9-abe8dcb27158`;
+    const headers = {
+      Authorization: `Bearer ${process.env.NVIDIA_API_KEY || ""}`,
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    };
 
-    const chat = model.startChat({
-      history: [
-        { role: "system", parts: "You're a good Russian friend." },
-        ...chatHistories.map(({ role, parts }) => ({ role, parts })),
+    const payload = {
+      messages: [
+        {
+          content: `Your name is ${Utility.client.user.displayName} discord bot and you exist to entertain or help people. you're not allowed to send NSFW stuffs and lasty you don't use too much emoji. You need to talk only in Russian lang.`,
+          role: "system",
+        },
+        ...(await this.GetAllChatHistory(chatId)),
       ],
-      generationConfig: { maxOutputTokens: 2048 },
+      stream: false,
+      max_tokens: 1024,
+    };
+
+    let response = await axios.post(url, payload, {
+      headers,
     });
 
-    const response = (await chat.sendMessage(input)).response;
+    while (response.status === 202) {
+      let requestId = response.headers["nvcf-reqid"];
+      let fetchUrl =
+        "https://api.nvcf.nvidia.com/v2/nvcf/pexec/status/" + requestId;
+      response = await axios.get(fetchUrl, { headers: headers });
+    }
 
-    await this.addChatHistory(chatId, "user", input);
-    await this.addChatHistory(chatId, "model", response.toString());
+    if (response.status !== 200) {
+      throw new Error(
+        `invocation failed with status ${response.status} ${response.data}`
+      );
+    }
 
-    embed.setDescription(`${response}`);
-    await message.edit({ embeds: [embed] });
+    await this.addChatHistory(
+      chatId,
+      "assistant",
+      response.data.choices[0].message.content
+    );
+
+    embed.setDescription(response.data.choices[0].message.content);
+
+    await message.edit({
+      embeds: [embed],
+    });
   }
 
-  private async addChatHistory(chatId: string, role: string, parts: string) {
+  public async addChatHistory(chatId: string, role: string, parts: string) {
     await Utility.prisma.chatHistory.create({
       data: {
         chat: { connect: { chatId } },
@@ -97,7 +131,10 @@ export class MessageCreate extends BaseModule {
       },
     });
 
-    return histories;
+    return histories.map(({ parts, role }) => ({
+      content: parts,
+      role,
+    }));
   }
 }
 
